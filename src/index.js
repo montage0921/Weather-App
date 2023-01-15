6d8b5efe25f170153cfcda2e9b6e40dfa6566ee4
montage0921 <garyshi0921@gmail.com>
2023-01-15 12:57:06 -0500

    Get correct time format for future hours

--- src/index.js
+++ src/index.js
@@ -18,36 +18,57 @@ searchBtn.addEventListener(`click`, function (e) {
 });
 
 async function getWeatherData(city) {
+  // get current weather data
   const responseCurrent = fetch(
     `http://api.weatherapi.com/v1/current.json?key=987f97d506dd4207a9f40954231201&q=${city}&aqi=no`
   );
+
+  // get future weather data
   const responseFuture = fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=987f97d506dd4207a9f40954231201&q=${city}&days=1&aqi=no&alerts=no`
   );
-  const responses = await Promise.all([responseCurrent, responseFuture]);
+
+  //get geoCoding (lat,lng) of the city
+  // this is used to get correct local time
+  const responseGeoCoding = fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCTnMU5cLNYyQJ93XqxsQf3DZVlnu5RJcE`
+  );
+
+  const responses = await Promise.all([
+    responseCurrent,
+    responseFuture,
+    responseGeoCoding,
+  ]);
+
   const currentWeatherAll = await responses[0].json();
   const futureWeatherAll = await responses[1].json();
+  const geoCode = await responses[2].json();
 
-  console.log(futureWeatherAll);
+  const { lat, lng } = geoCode.results[0].geometry.location;
 
+  //For future weather, only need for next 4 hours
   const futureWeather = futureWeatherAll.forecast.forecastday[0].hour.slice(
     0,
     4
   );
-  console.log(futureWeather);
 
-  //weather.time is this:
-  //2023-01-11 00:00, 2023-01-11 01:00...
-  //we ONLY need the hour and need to add AM or PM after the hour accordingly
-  const futureTime = futureWeather.map((weather) => {
-    const time = +weather.time.split(` `)[1].split(`:`)[0];
-    if (time < 12 && time !== 0) return `${time}AM`;
-    else if (time === 0) return `12AM`;
-    else return `${time}PM`;
-  });
+  // get a city's corresponding time zone
+  const responseTimeZone = await fetch(
+    `https://maps.googleapis.com/maps/api/timezone/json?location=${lat}%2C${lng}&timestamp=${futureWeather[0].time_epoch}&key=AIzaSyCTnMU5cLNYyQJ93XqxsQf3DZVlnu5RJcE`
+  );
+  const timeZone = await responseTimeZone.json();
 
-  console.log(futureTime);
+  //get time with AM or PM. e.g: 1AM, 5PM...
+  let date = new Date();
+  let options = { timeZone: timeZone.timeZoneId, hour: `numeric` };
+  let formatter = new Intl.DateTimeFormat([], options);
 
-  //   const result = await responseFuture.json();
-  //   console.log(result);
+  let currentHour = Number(formatter.format(date).split(` `)[0]);
+
+  const futureHours = [];
+  for (let i = 0; i <= 3; i++) {
+    currentHour++;
+    if (currentHour > 12) futureHours[i] = currentHour - 12 + `PM`;
+    else futureHours[i] = currentHour + `AM`;
+  }
 }
