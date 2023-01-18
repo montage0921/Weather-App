df277e9e8229f703c75063b08de99a3c7bfea3e7
montage0921 <garyshi0921@gmail.com>
2023-01-17 21:42:09 -0500

    Refactor Async Functions & fix a mistake of AM PM

--- src/index.js
+++ src/index.js
@@ -14,61 +14,102 @@ searchBtn.addEventListener(`click`, function (e) {
 
   let city = searchBar.value;
 
-  getWeatherData(city);
+  const currentWeatherPro = getCurrentWeather(city);
+
+  const futureWeatherPro = getFutureWeather(city);
+
+  const geoCodePro = getGeoAPIData(city);
+
+  const timeZonePro = getTimeZoneAPI(geoCodePro, futureWeatherPro);
+
+  getFutureHour(timeZonePro);
 });
 
-async function getWeatherData(city) {
+////////////////////Async function//////////
+//////////////////////////////////////////////
+
+//1. fetch current weather data from api
+async function getCurrentWeather(city) {
   // get current weather data
-  const responseCurrent = fetch(
+  const responseCurrent = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=987f97d506dd4207a9f40954231201&q=${city}&aqi=no`
   );
 
-  // get future weather data
-  const responseFuture = fetch(
+  const currentWeatherData = await responseCurrent.json();
+
+  return currentWeatherData;
+}
+
+//2. fetch future weather data from api
+async function getFutureWeather(city) {
+  const responseFuture = await fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=987f97d506dd4207a9f40954231201&q=${city}&days=1&aqi=no&alerts=no`
   );
 
-  //get geoCoding (lat,lng) of the city
-  // this is used to get correct local time
-  const responseGeoCoding = fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCTnMU5cLNYyQJ93XqxsQf3DZVlnu5RJcE`
-  );
+  const futureWeatherAll = await responseFuture.json();
 
-  const responses = await Promise.all([
-    responseCurrent,
-    responseFuture,
-    responseGeoCoding,
-  ]);
-
-  const currentWeatherAll = await responses[0].json();
-  const futureWeatherAll = await responses[1].json();
-  const geoCode = await responses[2].json();
-
-  const { lat, lng } = geoCode.results[0].geometry.location;
-
-  //For future weather, only need for next 4 hours
-  const futureWeather = futureWeatherAll.forecast.forecastday[0].hour.slice(
+  const futureWeatherData = futureWeatherAll.forecast.forecastday[0].hour.slice(
     0,
     4
   );
 
-  // get a city's corresponding time zone
-  const responseTimeZone = await fetch(
-    `https://maps.googleapis.com/maps/api/timezone/json?location=${lat}%2C${lng}&timestamp=${futureWeather[0].time_epoch}&key=AIzaSyCTnMU5cLNYyQJ93XqxsQf3DZVlnu5RJcE`
+  return futureWeatherData;
+}
+
+//3. get geo location info (lat,lng) from api
+async function getGeoAPIData(city) {
+  const responseGeoCode = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCTnMU5cLNYyQJ93XqxsQf3DZVlnu5RJcE`
   );
+
+  const geoCode = await responseGeoCode.json();
+
+  const { lat, lng } = geoCode.results[0].geometry.location;
+
+  return { lat, lng };
+}
+
+//4. get time zone of the city from api
+async function getTimeZoneAPI(geoCodePromise, futureWeather) {
+  const futureWeatherData = await futureWeather;
+
+  const timeEpoch = futureWeatherData[0].time_epoch;
+  const { lat, lng } = await geoCodePromise;
+
+  const responseTimeZone = await fetch(
+    `https://maps.googleapis.com/maps/api/timezone/json?location=${lat}%2C${lng}&timestamp=${timeEpoch}&key=AIzaSyCTnMU5cLNYyQJ93XqxsQf3DZVlnu5RJcE`
+  );
+
   const timeZone = await responseTimeZone.json();
 
-  //get time with AM or PM. e.g: 1AM, 5PM...
+  return timeZone;
+}
+
+async function getFutureHour(timeZonePro) {
+  const timeZone = await timeZonePro;
+
   let date = new Date();
   let options = { timeZone: timeZone.timeZoneId, hour: `numeric` };
   let formatter = new Intl.DateTimeFormat([], options);
 
   let currentHour = Number(formatter.format(date).split(` `)[0]);
+  let amOrPm = formatter.format(date).split(` `)[1];
 
   const futureHours = [];
+
   for (let i = 0; i <= 3; i++) {
     currentHour++;
-    if (currentHour > 12) futureHours[i] = currentHour - 12 + `PM`;
-    else futureHours[i] = currentHour + `AM`;
+
+    if (amOrPm == `AM`) {
+      if (currentHour > 12) futureHours[i] = currentHour - 12 + `PM`;
+      else if (currentHour == 12) futureHours[i] = `12PM`;
+      else futureHours[i] = currentHour + `AM`;
+    } else if (amOrPm == `PM`) {
+      if (currentHour > 12) futureHours[i] = currentHour - 12 + `AM`;
+      else if (currentHour == 12) futureHours[i] = `12AM`;
+      else futureHours[i] = currentHour + `PM`;
+    }
   }
+
+  return futureHours;
 }
